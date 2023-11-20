4b4f67a5354156382d258c3d1417c5abcf28c373
Milad Xandi <miladxandi@outlook.com>
2023-11-20 12:53:58 +0330

    Edited shared library

--- installer.sh
+++ installer.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 # Installing require updates
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y curl git build-essential libssl-dev libffi-dev libbz2-dev libreadline-dev libsqlite3-dev bluez libjpeg-dev liblzma-dev python3-tk zlib1g-dev autoconf libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata ffmpeg liblapack3 liblapack-dev python-tk python3-tk tk-dev
+#sudo apt update && sudo apt upgrade -y
+#sudo apt install -y curl git build-essential libssl-dev libffi-dev libbz2-dev libreadline-dev libsqlite3-dev bluez libjpeg-dev liblzma-dev python3-tk zlib1g-dev autoconf libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata ffmpeg liblapack3 liblapack-dev python-tk python3-tk tk-dev
 
 
 # Change user
 if ! id "dobo" &>/dev/null; then
   pass="1234"
   encrypted_password=$(echo -n "$pass" | openssl passwd -stdin -6)
-  sudo useradd -rm -p "$encrypted_password" dobo
+  sudo useradd -rm -p "$encrypted_password" dobo && mkdir /srv/dobo && chown dobo:dobo /srv/dobo
+  cd /srv/dobo || exit
   echo -e "We created a new user:\nUsername:dobo\nPassword:1234\nPlease rerun ths script!"
 else
-  sudo mkdir /srv/dobo
-  cd /srv/dobo || exit
-  sudo chown dobo:dobo /srv/dobo
   su - dobo -c '1234'
 fi
 
