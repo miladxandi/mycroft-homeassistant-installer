2541206d33b6d0d559e1cffba48acdf5da6443ef
Milad Xandi <miladxandi@outlook.com>
2023-11-20 12:56:15 +0330

    Edited shared library

--- installer.sh
+++ installer.sh
@@ -7,9 +7,10 @@
 
 # Change user
 if ! id "dobo" &>/dev/null; then
+  sudo mkdir /srv/dobo
   pass="1234"
   encrypted_password=$(echo -n "$pass" | openssl passwd -stdin -6)
-  sudo useradd -rm -p "$encrypted_password" dobo && mkdir /srv/dobo && chown dobo:dobo /srv/dobo
+  sudo useradd -rm -p "$encrypted_password" dobo && chown dobo:dobo /srv/dobo
   cd /srv/dobo || exit
   echo -e "We created a new user:\nUsername:dobo\nPassword:1234\nPlease rerun ths script!"
 else
