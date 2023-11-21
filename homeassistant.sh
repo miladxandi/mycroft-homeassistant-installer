74ea25896180bbb6affab8dbc5d10b2ddb13bfda
Milad Xandi <miladxandi@outlook.com>
2023-11-21 15:10:47 +0330

    Edited shared library

--- homeassistant.sh
+++ homeassistant.sh
@@ -3,9 +3,18 @@ current_directory=$(pwd)
 # Back to the previous directory
 cd /srv/dobo || exit
 
+# Adding Pyenv to the file path of profile to run in any session
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
 # Changing Python version
 pyenv global 3.11.0
 python3.11 -m pip install --upgrade pip
+python3.11 -m venv home_assistant
 # Installation of  Home Asistant
 echo "Installing Home Assistant Core..."
 python -m venv home_assistant
